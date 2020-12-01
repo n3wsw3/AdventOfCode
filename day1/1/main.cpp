46d2643887b97d03fc84407d8ab83ed38d3e496f
Simon Uggla <simon.uggla@aol.com>
2020-12-01 09:39:59 +0000

    Updated formatting of cpp files

--- day1/1/main.cpp
+++ day1/1/main.cpp
@@ -2,25 +2,20 @@
 #include <iostream>
 #include <vector>
 
-int main()
-{
-    std::vector<int> input;
-    std::ifstream is("../input.in");
-    int x;
-    while (is >> x)
-    {
-        input.push_back(x);
-    }
+int main() {
+  std::vector<int> input;
+  std::ifstream is("../input.in");
+  int x;
+  while (is >> x) {
+	input.push_back(x);
+  }
 
-    for (int i = 0; i < input.size(); i++)
-    {
-        for (int j = 0; j < input.size(); j++)
-        {
-            if (input[i] + input[j] == 2020)
-            {
-                std::cout << "Answer: " << input[i] * input[j] << std::endl;
-                return 0;
-            }
-        }
-    }
+  for (int i = 0; i < input.size(); i++) {
+	for (int j = 0; j < input.size(); j++) {
+	  if (input[i] + input[j] == 2020) {
+		std::cout << "Answer: " << input[i] * input[j] << std::endl;
+		return 0;
+	  }
+	}
+  }
 }
