773bab8dd06e280476c3fe5fd802fb558f489a8b
Simon Uggla <simon.uggla@aol.com>
2020-12-02 22:32:04 +0000

    Changed day 1 to measure execution time

--- day1/1/main.cpp
+++ day1/1/main.cpp
@@ -1,21 +1,30 @@
 #include <fstream>
 #include <iostream>
+#include "../../helper/measure.h"
 #include <vector>
 
-int main() {
-  std::vector<int> input;
-  std::ifstream is("../input.in");
-  int x;
-  while (is >> x) {
-	input.push_back(x);
-  }
-
-  for (int i = 0; i < input.size(); i++) {
-	for (int j = 0; j < input.size(); j++) {
-	  if (input[i] + input[j] == 2020) {
-		std::cout << "Answer: " << input[i] * input[j] << std::endl;
-		return 0;
-	  }
+int addTo2020(std::vector<int> input)
+{
+	for (int i = 0; i < input.size(); i++) {
+		for (int j = 0; j < input.size(); j++) {
+			if (input[i] + input[j] == 2020) {
+				return input[i] * input[j];
+			}
+		}
 	}
-  }
+	return -1;
+}
+
+int main()
+{
+	std::vector<int> input;
+	std::ifstream is("../input.in");
+	int x;
+	while (is >> x) {
+		input.push_back(x);
+	}
+
+	auto answer = measure::function(addTo2020, input);
+	std::cout << "Answer: " << std::get<0>(answer) << " in "
+		  << std::get<1>(answer).count() << " μs" << std::endl;
 }
--- day1/2/main.cpp
+++ day1/2/main.cpp
@@ -1,24 +1,36 @@
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include "../../helper/measure.h"
+#include <tuple>
 
-int main() {
-  std::vector<int> input;
-  std::ifstream is("../input.in");
-  int x;
-  while (is >> x) {
-	input.push_back(x);
-  }
-
-  for (int i = 0; i < input.size(); i++) {
-	for (int j = 0; j < input.size(); j++) {
-	  for (int k = 0; k < input.size(); k++) {
-		if (input[i] + input[j] + input[k] == 2020) {
-		  std::cout << "Answer: " << input[i] * input[j] * input[k]
-					<< std::endl;
-		  return 0;
+int addTo2020(std::vector<int>& input)
+{
+	for (int i = 0; i < input.size(); i++) {
+		for (int j = 0; j < input.size(); j++) {
+			for (int k = 0; k < input.size(); k++) {
+				if (input[i] + input[j] + input[k] == 2020) {
+					return input[i] * input[j] * input[k];
+				}
+			}
 		}
-	  }
 	}
-  }
+
+	return -1;
+}
+
+int main()
+{
+	std::vector<int> input;
+	std::ifstream is("../input.in");
+	int x;
+	while (is >> x) {
+		input.push_back(x);
+	}
+
+	auto answer = measure::function(addTo2020, input);
+	std::cout << "Answer: " << std::get<0>(answer) << " in "
+		  << std::get<1>(answer).count() << " μs" << std::endl;
+	return 0;
 }
